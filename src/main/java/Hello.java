f0f44b01945e14224ac77bc4f288f0585c4305f9
Xxl <Xxl@com>
2022-10-06 12:00:02 +0800

    master fourth commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -6,5 +6,8 @@ public class Hello {
         System.out.println("hello 你好 ");
         System.out.println("=======");
         System.out.println("此处为hot分支");
+        System.out.println("=======");
+        System.out.println("此处为master分支");
+
     }
 }
