5ef8728f199e551ec2283b531623fabe24addd15
Xxl <Xxl@com>
2022-10-06 11:36:37 +0800

    second commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,8 @@
- /**
- * @author XueLong
- * @Description
- * @create 2022-10-2022/10/6-11:28
- */
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world ");
+        System.out.println("hello github ");
+
     }
 }
