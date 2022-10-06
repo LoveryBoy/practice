888d7868c1f5cc398e930e5b323f0c4cc2c99bf1
Xxl <Xxl@com>
2022-10-06 11:33:37 +0800

    first commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+ /**
+ * @author XueLong
+ * @Description
+ * @create 2022-10-2022/10/6-11:28
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world ");
+    }
+}
